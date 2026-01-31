e3814e135999480a4ad506135607a322fad616a7
MLandis-4 <max.landis@student.northampton.edu>
2026-01-31 17:13:57 +0000

    I created a program to take a width and length of a rectangle and find it's area and perimeter. I did so using doubles and cout code. The Terminal then produces exactly what was in the instructions.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+//: Max Landis
+
 #include <iostream>
 using namespace std;
 
@@ -5,5 +7,18 @@ int main() {
   const double length = 7.2;
   const double width = 1.3;
 
+  double perimeter;
+  double area;
+
+  cout << "Rectangle Properties:" << endl;
+
+  cout << "Length = " << length << endl;
+  cout << "Width = " << width << endl;
+
+  area = length*width;
+  cout << "Area = " << area << endl;
+
+  perimeter = 2*(length+width);
+  cout << "Perimeter = " << perimeter << endl;
 
 }
